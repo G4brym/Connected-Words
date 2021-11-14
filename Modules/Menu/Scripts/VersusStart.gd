9762b003b182858ce7ae3a8bd08b7c281315f1cb
Gabriel Massadas <5445926+G4brym@users.noreply.github.com>
2021-11-14 22:54:04 +0000

    Start to build android plugin to handle intent

--- Modules/Menu/Scripts/VersusStart.gd
+++ Modules/Menu/Scripts/VersusStart.gd
@@ -21,4 +21,8 @@ func _on_Continue_pressed():
 	var url = "https://apichain.dev.catappult.io/transaction/eskills?value=1&currency=USD&product=1v1" + \
 				"&user_id=a25cd286&domain=com.massadas.connectedwords&environment=SANDBOX&number_of_users=2&user_name=" + \
 				$"Username".text
-	OS.shell_open(url)
+				
+	if Engine.has_singleton("GodotEskills"):
+		var singleton = Engine.get_singleton("GodotEskills")
+		singleton.startMatch(url)
+	#OS.shell_open(url)
